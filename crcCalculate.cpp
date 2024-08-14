1639608ac6f41b86faec20969504df21d22d9cfe
Ritesh P <riteshp@tataadvancedsystems.com>
2024-08-14 10:50:11 +0530

    Refactoring and Commentation of Codebase

--- crcCalculate.cpp
+++ crcCalculate.cpp
@@ -1,136 +1,112 @@
 #include "crcCalculate.h"
 #include "ui_crccalculate.h"
 
+// Constructor
 crcCalculate::crcCalculate(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::crcCalculate)
 {
     ui->setupUi(this);
 
-    //Stylesheet
-    QFile *qss = new QFile(":/styleSheet.qss");
-    if(!qss->exists()){
-        DEBUG("Unable to find StyleSheet File.")
+    // Load and apply the stylesheet
+    QFile qss(":/styleSheet.qss");
+    if (!qss.exists()) {
+        DEBUG("Unable to find StyleSheet File.");
         return;
     }
-    if(!qss->open(QFile::ReadOnly)){
-        DEBUG("Unable to Open StyleSheet File.")
+    if (!qss.open(QFile::ReadOnly)) {
+        DEBUG("Unable to Open StyleSheet File.");
         return;
     }
-    this->setStyleSheet(QString(qss->readAll()));
-    qss->close();
+    this->setStyleSheet(QString(qss.readAll()));
+    qss.close();
 
+    // Set window properties
     this->setWindowIcon(QIcon(":/crc_label.svg"));
-    this->setWindowTitle("Crc Checksum Calculator");
-
+    this->setWindowTitle("CRC Checksum Calculator");
 }
 
+// Destructor
 crcCalculate::~crcCalculate()
 {
     delete ui;
 }
 
-
-void crcCalculate::CalculateChecksum(char *checksum,QString filePath)
+// Calculate the CRC checksum of a file
+void crcCalculate::CalculateChecksum(char *checksum, QString filePath)
 {
-    qint64 size;
-    unsigned char * memblock;
-    unsigned short checkSum;
+    QFile file(filePath);
+    if (file.open(QIODevice::ReadOnly)) {
+        DEBUG("File for Checksum calculation [" + filePath + "] opened successfully.");
 
-    QString fileName(filePath);
-    QFile file(fileName);
-    if (file.open(QIODevice::ReadOnly) == true)
-    {
-        DEBUG("File for Checksum calculation ["+fileName+"] opened successfully.")
-        /* file is opened */
+        // Get file size
+        qint64 size = file.size();
 
-        /* get the size of file */
-        size = file.size();
+        // Allocate memory to read the file content
+        auto memblock = std::make_unique<unsigned char[]>(static_cast<size_t>(size));
 
-        /* Assign memory to memory block variable */
-        memblock = new unsigned char [static_cast<unsigned long long>(size)];
+        // Read the file into memory
+        file.seek(0);
+        file.read(reinterpret_cast<char*>(memblock.get()), size);
 
-        /* Goto 0 position of the file */
-        file.seek (0);
+        // Compute the CRC checksum
+        unsigned short checkSum = computeCrc(memblock.get(), static_cast<int>(size));
 
-        /* Read the file to new memory block */
-        file.read (reinterpret_cast<char*>(memblock), size);
+        // Store the result in the provided checksum variable
+        sprintf(checksum, "%04x", checkSum);
 
-        /* Update global store with returned checksum */
-        checkSum = computeCrc(memblock,static_cast<int>(size));
-
-        sprintf(reinterpret_cast<char*>(checksum),"%04x",checkSum);
-        /* Close the file */
+        // Close the file
         file.close();
 
-        /* Delete the memory block */
-        delete[] memblock;
-
-    }
-    else
-    {
-        DEBUG("Unable to Open ["+fileName+"] File. For Checksum Calculation.")
+    } else {
+        DEBUG("Unable to Open [" + filePath + "] File for Checksum Calculation.");
     }
 }
 
-
-
-//crc checksum function
-
-
+// Compute the CRC checksum of a message
 unsigned short crcCalculate::computeCrc(unsigned char *message, int blk_len)
 {
     unsigned short crctable[256] = {0x0000, 0x8005, 0x800F, 0x000A, 0x801B, 0x001E, 0x0014, 0x8011,0x8033, 0x0036, 0x003C, 0x8039, 0x0028, 0x802D, 0x8027, 0x0022,0x8063, 0x0066, 0x006C, 0x8069, 0x0078, 0x807D, 0x8077, 0x0072,0x0050, 0x8055, 0x805F, 0x005A, 0x804B, 0x004E, 0x0044, 0x8041,0x80C3, 0x00C6, 0x00CC, 0x80C9, 0x00D8, 0x80DD, 0x80D7, 0x00D2,0x00F0, 0x80F5, 0x80FF, 0x00FA, 0x80EB, 0x00EE, 0x00E4, 0x80E1,0x00A0, 0x80A5, 0x80AF, 0x00AA, 0x80BB, 0x00BE, 0x00B4, 0x80B1,0x8093, 0x0096, 0x009C, 0x8099, 0x0088, 0x808D, 0x8087, 0x0082,0x8183, 0x0186, 0x018C, 0x8189, 0x0198, 0x819D, 0x8197, 0x0192,0x01B0, 0x81B5, 0x81BF, 0x01BA, 0x81AB, 0x01AE, 0x01A4, 0x81A1,0x01E0, 0x81E5, 0x81EF, 0x01EA, 0x81FB, 0x01FE, 0x01F4, 0x81F1,0x81D3, 0x01D6, 0x01DC, 0x81D9, 0x01C8, 0x81CD, 0x81C7, 0x01C2,0x0140, 0x8145, 0x814F, 0x014A, 0x815B, 0x015E, 0x0154, 0x8151,0x8173, 0x0176, 0x017C, 0x8179, 0x0168, 0x816D, 0x8167, 0x0162,0x8123, 0x0126, 0x012C, 0x8129, 0x0138, 0x813D, 0x8137, 0x0132,0x0110, 0x8115, 0x811F, 0x011A, 0x810B, 0x010E, 0x0104, 0x8101,0x8303, 0x0306, 0x030C, 0x8309, 0x0318, 0x831D, 0x8317, 0x0312,0x0330, 0x8335, 0x833F, 0x033A, 0x832B, 0x032E, 0x0324, 0x8321,0x0360, 0x8365, 0x836F, 0x036A, 0x837B, 0x037E, 0x0374, 0x8371,0x8353, 0x0356, 0x035C, 0x8359, 0x0348, 0x834D, 0x8347, 0x0342,0x03C0, 0x83C5, 0x83CF, 0x03CA, 0x83DB, 0x03DE, 0x03D4, 0x83D1, 0x83F3, 0x03F6, 0x03FC, 0x83F9, 0x03E8, 0x83ED, 0x83E7, 0x03E2,0x83A3, 0x03A6, 0x03AC, 0x83A9, 0x03B8, 0x83BD, 0x83B7, 0x03B2,0x0390, 0x8395, 0x839F, 0x039A, 0x838B, 0x038E, 0x0384, 0x8381,0x0280, 0x8285, 0x828F, 0x028A, 0x829B, 0x029E, 0x0294, 0x8291,0x82B3, 0x02B6, 0x02BC, 0x82B9, 0x02A8, 0x82AD, 0x82A7, 0x02A2,0x82E3, 0x02E6, 0x02EC, 0x82E9, 0x02F8, 0x82FD, 0x82F7, 0x02F2,0x02D0, 0x82D5, 0x82DF, 0x02DA, 0x82CB, 0x02CE, 0x02C4, 0x82C1,0x8243, 0x0246, 0x024C, 0x8249, 0x0258, 0x825D, 0x8257, 0x0252,0x0270, 0x8275, 0x827F, 0x027A, 0x826B, 0x026E, 0x0264, 0x8261,0x0220, 0x8225, 0x822F, 0x022A, 0x823B, 0x023E, 0x0234, 0x8231,0x8213, 0x0216, 0x021C, 0x8219, 0x0208, 0x820D, 0x8207, 0x0202};
 
-    unsigned char index;/*  index for  the crctable array (previously int)*/
-    unsigned long crc=0L;/*  Return value */
-    unsigned char  *blk_adr;/*  Pointer to the message block (previously char)*/
+    unsigned long crc = 0L;
+    unsigned char *blk_adr = message;
 
-    /*  Initialize  blk_adr pointer to point to message bytes  */
-    blk_adr = message;
-
-    /*  Repeat the following steps for all the message bytes */
-    while(blk_len != 0)
-    {
-
-        /*  Compute the index of the  requisite entry of the crctable */
-        index=((crc>>8) ^ *blk_adr++) & 0xFFL;
-
-        /*  Compute the crc  using the computed index and the previous crc value.*/
-        crc=crctable[index] ^ (crc <<8);
+    // Iterate over the message bytes to compute the checksum
+    while (blk_len != 0) {
+        unsigned char index = ((crc >> 8) ^ *blk_adr++) & 0xFFL;
+        crc = crctable[index] ^ (crc << 8);
         blk_len--;
     }
 
-    return (static_cast<unsigned short>(crc));
+    return static_cast<unsigned short>(crc);
 }
 
-
-
+// Slot triggered when the CRC calculation button is clicked
 void crcCalculate::on_crcButton_clicked()
 {
     ui->crcLabel->setTextFormat(Qt::RichText);
 
-    // Open file dialog for executable files
-        QString fileName = QFileDialog::getOpenFileName(this, tr("Open Executable"), "",
-                                        tr("Executable Files (*.exe);;All Files (*)"));
+    // Open file dialog to select an executable file
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Executable"), "",
+                                                    tr("Executable Files (*.exe);;All Files (*)"));
 
-        // Check if a file was selected
-        if (fileName.isEmpty()) {
-            ui->crcLabel->setText(tr("Error in Selecting Application"));
-            return;
-        }
+    // Check if a file was selected
+    if (fileName.isEmpty()) {
+        ui->crcLabel->setText(tr("Error in Selecting Application"));
+        return;
+    }
 
-        // Here you would calculate the checksum of the file
-        char checksum[5];
+    // Calculate the checksum of the selected file
+    char checksum[5];
+    CalculateChecksum(checksum, fileName);
 
-        CalculateChecksum(checksum,fileName);
+    // Get the name of the executable
+    QString exeName = QFileInfo(fileName).fileName();
 
-        QString exeName = QFileInfo(fileName).fileName();
+    // Format the output message
+    QString message = tr("Checksum of %1 is: <font color='blue'>%2</font>")
+                         .arg(exeName, QString(checksum));
 
-        // Format the output message
-        QString message = tr("Checksum of %1 is: <font color='blue'>%2</font>")
-                             .arg(exeName, QString(checksum));
-
-       // Display the result in the label
-       ui->crcLabel->setText(message);
+    // Display the result in the label
+    ui->crcLabel->setText(message);
 }
